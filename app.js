f852fdd112caa4f013d2294ffb4777ea43a3b082
Juanma Barcia <37333385+JuanmaBarcia@users.noreply.github.com>
2021-07-09 16:37:11 +0200

    deleted what is unnecessary

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
-// require("dotenv").config();
 var cors = require("cors");
 
-const router = require("./routes/pages");
 const routerApi = require("./routes/products");
 require("./utils/db"); // Lanza la BBDD mongoose
 
@@ -11,39 +9,14 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-// Motor de vistas
-app.set("view engine", "pug");
-app.set("views", "./views");
-
 // Para habilitar recepcion de JSONs
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Para acceder con API KEY: http://localhost:3000/products?id=4&API_KEY=123abcd
-// http://localhost:3000?API_KEY=123abcd
-
-function isAdmin(req, res, next) {
-  if (req.query.API_KEY == "123abcd") {
-    next();
-  } else {
-    res
-      .status(403)
-      .send(
-        `Sorry but you are not an admin and you do not have access to route ${req.url}`
-      );
-  }
-}
-// Middleware para controlar acceso
-// Descomentar para probar middelware
-//app.use(isAdmin);
 
 //API
 //http://localhost:3000/api/products
 //http://localhost:3000/api/products?id=2
 app.use("/api", routerApi); // rutas para API
-
-// WEB: usar el Router
-app.use("/", router); // rutas para WEB
 
 const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
